2e2c6a57ff5aea0fbfa3ab7a70b980608d7f3033
sunnivaschj <114073989+sunnivaschj@users.noreply.github.com>
2024-05-03 14:46:19 +0200

    made capitalize and test_capitalize

--- test_capitalize.py
+++ test_capitalize.py
@@ -1,8 +1,14 @@
 import pytest
 # Fin måte å lage unit tests
+# TDE test driven enviroment
+from capitalize import *
 
 
+def test_capital_case():
+  assert capitalize_case('elvebakken') == 'Elvebakken'
 
+def test_capital_case():
+  assert capitalize_case('elvebakken') != 'elvebakken'
 
 
 
